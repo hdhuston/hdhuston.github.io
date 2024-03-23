a95c62076c1963cf2b6b9ea7800822ffa8bd986b
hdhuston <hhuston@nd.edu>
2024-03-23 11:30:31 -0400

    Try again

---
+++ Intro.py
@@ -0,0 +1,9 @@
+from colorama import Fore
+
+
+print('Hello!')
+print('My name is Hannah Huston and I am a sophomore computer science major at Notre Dame')
+print('I am interested in finding the connections between data science and political science')
+print( Fore.BLUE + 'My email is hhuston@nd.edu')
+print('My linkedin is https://www.linkedin.com/in/hannah-huston-329973262/')
+
